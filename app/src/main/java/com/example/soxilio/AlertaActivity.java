a927071112882a7ed9594365ea6f1f8a02d0c23b
JPabloMNKS <jpablo.bedoya7@gmail.com>
2019-03-30 09:40:44 -0400

    GPS aumentado mandando por Toast

--- app/src/main/java/com/example/soxilio/AlertaActivity.java
+++ app/src/main/java/com/example/soxilio/AlertaActivity.java
@@ -1,9 +1,15 @@
 package com.example.soxilio;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.Uri;
+import android.os.Handler;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
@@ -12,10 +18,19 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
-public class AlertaActivity extends AppCompatActivity implements View.OnClickListener {
+public class AlertaActivity extends AppCompatActivity implements View.OnClickListener, LocationListener {
 
     CardView cardOne, cardTwo, cardThree, cardFour;
     private static final int PERMISSION_REQUEST_CODE = 1;
+    private String mensaje = "";
+
+    //gps
+    protected LocationManager locationManager;
+
+    //Tiempo
+    private Handler mHandler = new Handler();
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +61,20 @@ public class AlertaActivity extends AppCompatActivity implements View.OnClickLis
 
             }
         }
+
+        // GPS
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,}, 1000);
+        } else {
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, this);
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0,  this);
+        }
+
+
+
     }
 
     @Override
@@ -61,7 +90,7 @@ public class AlertaActivity extends AppCompatActivity implements View.OnClickLis
                 break;
 
             case R.id.card_three:
-
+                startRepeating();
                 break;
 
             case R.id.card_four:
@@ -97,5 +126,52 @@ public class AlertaActivity extends AppCompatActivity implements View.OnClickLis
             e.printStackTrace();
         }
     }
+
+    // GPS
+
+    @Override
+    public void onLocationChanged(Location location) {
+        mensaje = "Mi ubicacion actual es Latitud: " +location.getLatitude()
+                +"Longitud: "+location.getLongitude();
+
+//        Toast.makeText(getApplicationContext(),"Mi ubicacion actual es Latitud: "
+  //      + location.getLatitude()+" Longitud: "+ location.getLongitude(),Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+
+    // Repetidor
+
+    public void startRepeating(){
+//        mHandler.postDelayed(mToasRunnable,5000);
+        mToasRunnable.run();
+    }
+
+    public void stopRepeating(){
+        mHandler.removeCallbacks(mToasRunnable);
+    }
+
+    private Runnable mToasRunnable = new Runnable() {
+        @Override
+        public void run() {
+            Toast.makeText(getApplicationContext(),mensaje,Toast.LENGTH_SHORT).show();
+            mHandler.postDelayed(this,5000);
+
+        }
+    };
+
 
 }
